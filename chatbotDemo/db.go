32ab2d0cfc8dcb27fcf53636650dea09beaad27a
Shunichi1100921 <shunichi1100921@icloud.com>
2023-11-29 08:38:47 +0900

    refactor: concat write_db.go and read_db.go into db.go.

--- chatbotDemo/db.go
+++ chatbotDemo/db.go
@@ -58,3 +58,18 @@ func ShowJSON() {
 
 	fmt.Println(b)
 }
+
+func SaveContent(content Content) {
+	dataSourceName := "root:password@tcp(localhost:3306)/demoSQL"
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		fmt.Printf("DB Connection error %v\n", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO demotable (answer, feedback) VALUES (?, ?)", content.Answer, content.Feedback)
+	if err != nil {
+		fmt.Printf("DB Exec error %v\n", err)
+	}
+	fmt.Println("Content inserted successfully.")
+}
