742d0bb4038bf3c6bb3278050da66bdf599b7561
Shunichi1100921 <shunichi1100921@icloud.com>
2023-11-29 08:37:18 +0900

    feat: create the function to read content from database and show json from content.

--- chatbotDemo/read_db.go
+++ chatbotDemo/read_db.go
@@ -1,1 +1,60 @@
 package chatbotDemo
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type DBContent struct {
+	ID        int
+	Answer    string
+	Feedback  string
+	CreatedAt time.Time
+}
+
+func ReadContent(content Content) {
+	dataSourceName := "root:password@tcp(localhost:3306)/demoSQL"
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		fmt.Printf("DB Connection error %v\n", err)
+	}
+	defer db.Close()
+
+	// Ping make sure connection to DB
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("DB Ping error %v\n", err)
+	}
+
+	rows, err := db.Query("SELECT * FROM demotable")
+	if err != nil {
+		fmt.Printf("DB Query error %v\n", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			answer   string
+			feedback string
+		)
+		if err = rows.Scan(&answer, &feedback); err != nil {
+			fmt.Printf("DB Scan error %v\n", err)
+		}
+		fmt.Printf("")
+	}
+
+}
+
+func ShowJSON() {
+	c := Content{"answer", "feadback"}
+
+	b, err := json.Marshal(c)
+	if err != nil {
+		fmt.Printf("JSON Decoding error %v\n", err)
+	}
+
+	fmt.Println(b)
+}
