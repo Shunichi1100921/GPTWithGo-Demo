0312c86e3a8e4f9b6162af99b23c91f56929c9d1
Shunichi1100921 <shunichi1100921@icloud.com>
2023-12-17 20:05:36 +0100

    feat: add JSON chat API. #1

--- chatBotAPI/chat.go
+++ chatBotAPI/chat.go
@@ -11,10 +11,10 @@ func createOpenAIClient(apiKey string) *openai.Client {
 	return openai.NewClient(apiKey)
 }
 
-func getOrCreateChatMessages(chatInput ChatInput) []openai.ChatCompletionMessage {
+func getOrCreateChatMessages(chatInput ChatInput, stream bool) []openai.ChatCompletionMessage {
 	chatHistory := getChatHistory(chatInput)
 
-	messages := createInitialMessage()
+	messages := createInitialMessage(stream)
 
 	for _, h := range chatHistory {
 		messages = append(messages, openai.ChatCompletionMessage{
@@ -35,22 +35,39 @@ func getOrCreateChatMessages(chatInput ChatInput) []openai.ChatCompletionMessage
 	return messages
 }
 
-func createInitialMessage() []openai.ChatCompletionMessage {
+func createInitialMessage(stream bool) []openai.ChatCompletionMessage {
+	var firstPrompt string
+	if stream {
+		firstPrompt = "Answer my questions."
+	} else {
+		firstPrompt = "You're the boss of me who answers my questions.  You gotta give me the answers and feedback " +
+			"about the way I asked. You should return in JSON format, which has two keys.  One is the answer and " +
+			"the other is feedback."
+	}
 	return []openai.ChatCompletionMessage{
 		{
 			Role:    openai.ChatMessageRoleUser,
-			Content: "Answer my question.",
+			Content: firstPrompt,
 		},
 	}
 }
 
-func createChatRequest(chatInput ChatInput) openai.ChatCompletionRequest {
-	messages := getOrCreateChatMessages(chatInput)
-	return openai.ChatCompletionRequest{
-		Model:     openai.GPT3Dot5Turbo,
-		MaxTokens: 1000,
-		Messages:  messages,
-		Stream:    true,
+func createChatRequest(chatInput ChatInput, stream bool) openai.ChatCompletionRequest {
+	messages := getOrCreateChatMessages(chatInput, stream)
+	if stream {
+		return openai.ChatCompletionRequest{
+			Model:     openai.GPT3Dot5Turbo,
+			MaxTokens: 1000,
+			Messages:  messages,
+			Stream:    stream,
+		}
+	} else {
+		return openai.ChatCompletionRequest{
+			Model:          openai.GPT3Dot5Turbo1106,
+			MaxTokens:      1000,
+			Messages:       messages,
+			ResponseFormat: &openai.ChatCompletionResponseFormat{Type: openai.ChatCompletionResponseFormatTypeJSONObject},
+		}
 	}
 }
 
@@ -59,8 +76,21 @@ func CreateChatCompletionStream(chatInput ChatInput) (stream *openai.ChatComplet
 	client := createOpenAIClient(apiKey)
 	ctx := context.Background()
 
-	request := createChatRequest(chatInput)
+	request := createChatRequest(chatInput, true)
 	stream, err = client.CreateChatCompletionStream(ctx, request)
+	if err != nil {
+		log.Fatalf("Error creating chat completion: %v\n", err)
+	}
+	return
+}
+
+func CreateChatCompletionJSON(chatInput ChatInput) (response openai.ChatCompletionResponse, err error) {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	client := createOpenAIClient(apiKey)
+	ctx := context.Background()
+
+	request := createChatRequest(chatInput, false)
+	response, err = client.CreateChatCompletion(ctx, request)
 	if err != nil {
 		log.Fatalf("Error creating chat completion: %v\n", err)
 	}
