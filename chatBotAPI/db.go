1fe535bbb12da88b283de6472c08ddccf90ff795
Shunichi1100921 <shunichi1100921@icloud.com>
2023-12-18 10:32:45 +0100

    feat: add API to read and save JSON chat history. #1

--- chatBotAPI/db.go
+++ chatBotAPI/db.go
@@ -50,3 +50,40 @@ func SaveChatHistory(chatInput ChatInput, finalResponse string) {
 	}
 	log.Println("Chat history was saved successfully.")
 }
+
+func getChatWithFeedbackHistory(chatInput ChatInput) []ChatHistory {
+	db := connectDB()
+	if err := db.Ping(); err != nil {
+		log.Fatalf("DB Ping Error %v\n", err)
+	}
+
+	rows, err := db.Query("SELECT user_prompt, bot_response FROM demoSQL.chatWithFeedbackHistory WHERE id = ?", chatInput.ChatID)
+	if err != nil {
+		log.Fatalf("DB Query error %v\n", err)
+	}
+	defer rows.Close()
+
+	var chatHistories []ChatHistory
+
+	for rows.Next() {
+		var h ChatHistory
+		if err = rows.Scan(&h.UserPrompt, &h.BotResponse); err != nil {
+			log.Fatalf("DB Scan error %v\n", err)
+		}
+		chatHistories = append(chatHistories, h)
+	}
+	return chatHistories
+}
+
+func SaveChatHistoryWithFeedback(chatInput ChatInput, response JSONChatResponse) {
+	db := connectDB()
+	if err := db.Ping(); err != nil {
+		log.Fatalf("DB Ping Error %v\n", err)
+	}
+
+	_, err := db.Exec("INSERT INTO demoSQL.chatWithFeedbackHistory (id, user_prompt, bot_response, feedback) VALUES (?, ?, ?, ?)", chatInput.ChatID, chatInput.Message, response.Answer, response.Feedback)
+	if err != nil {
+		log.Fatalf("DB Exec error %v\n", err)
+	}
+	log.Println("Chat history was saved successfully.")
+}
