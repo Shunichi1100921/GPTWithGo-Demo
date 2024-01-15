8f1273e39838e185179008e0fef157a088ce33c9
Shunichi1100921 <shunichi1100921@icloud.com>
2024-01-15 18:03:08 +0100

    feat: create the page to show stream chat history. #2

---
+++ frontend/chat-bot/app/chatbot/stream/[chatId]/history/page.tsx
@@ -0,0 +1,35 @@
+// 'use client'
+// import React, { useEffect, useState } from "react";
+import { fetchStreamChatHistory } from "@/app/lib/chatService";
+
+interface ChatEntry {
+    user_prompt: string;
+    bot_response: string;
+}
+
+export default async function Page({ params }: { params: { chatId: string }} ) {
+    const chatHistory: ChatEntry[] = await fetchStreamChatHistory(params.chatId)
+
+    function renderChatHistory() {
+        if (chatHistory) {
+            return chatHistory.map((entry, index) => (
+                <div key={index}>
+                    <p>{typeof entry}</p>
+                    <p>User: {entry.user_prompt}</p>
+                    <p>Bot: {entry.bot_response}</p>
+                </div>
+            ));
+        } else {
+            return <p>No chat with this id.</p>
+        }
+    }
+
+    return (
+        <div>
+            <h1>Stream Chat History for Chat ID: {params.chatId}</h1>
+            <div>
+                {renderChatHistory()}
+            </div>
+        </div>
+    );
+};
