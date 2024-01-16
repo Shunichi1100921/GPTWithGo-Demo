4fd39cdf4f8d3dc389d8f8c5a80b88e5897be857
Shunichi1100921 <shunichi1100921@icloud.com>
2024-01-16 16:02:20 +0100

    feat: install cors in gin api for frontend server to access them. #2

--- backend/cmd/webApp/main.go
+++ backend/cmd/webApp/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"demo/web/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
-
-	"demo/web/handlers"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/chat/stream", handlers.HandleStreamChat)
 	r.POST("/chat/json", handlers.HandleJSONChat)
