6e7287d1dddbfd5ee712c454b56a020526c427d6
Shunichi1100921 <shunichi1100921@icloud.com>
2024-01-17 18:52:32 +0100

    fix: the bug that the message user sent doesn't show in chat screen. to store the chatLog, create ChatInterface component. #2

--- frontend/chat-bot/app/chatbot/json/[chatId]/chat/page.tsx
+++ frontend/chat-bot/app/chatbot/json/[chatId]/chat/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { JSONChatHistory } from "@/app/ui/chat-history";
-import ChatInput from "@/app/ui/chat-input";
+import ChatInterface from "@/app/ui/chat-interface";
+import {JSONChatHistory} from "@/app/ui/chat-history";
 
 export default function Page({ params }: { params: { chatId: string }}) {
     return (
-        <div>
-            <h1>JSON Chat for Chat ID: {params.chatId}</h1>
-            <JSONChatHistory chatId={params.chatId}/>
-            <ChatInput chatId={params.chatId}/>
+        <div className="flex flex-col h-screen">
+            <div className="p-3 bg-gray-800 text-white">
+                <h1>JSON Chat for Chat ID: {params.chatId}</h1>
+                <JSONChatHistory chatId={params.chatId}/>
+                <ChatInterface chatId={params.chatId}/>
+            </div>
         </div>
     );
 }
---
+++ frontend/chat-bot/app/ui/chat-interface.tsx
@@ -0,0 +1,64 @@
+'use client';
+import ChatInput  from "@/app/ui/chat-input";
+import React, {useState} from "react";
+import {sendMessageJSONChat} from "@/app/lib/chatService";
+import {revalidatePath} from "next/cache";
+import {redirect} from "next/navigation";
+
+
+export interface ChatMessage {
+    sender: 'user' | 'bot';
+    message: string;
+}
+
+export default function ChatInterface({ chatId }: { chatId: string }) {
+
+    const [chatLog, setChatLog] = useState<ChatMessage[]>([]);
+    const [currentMessage, setCurrentMessage] = useState<string>("");
+
+    const handleSendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (currentMessage === "") {
+            return;
+        }
+
+        console.log("Sending message: ", currentMessage);
+        setChatLog([...chatLog, {sender: 'user', message: currentMessage}])
+        try {
+            const response = await sendMessageJSONChat(chatId, currentMessage);
+            setChatLog([
+                ...chatLog,
+                {sender: 'user', message: currentMessage},
+                {sender: 'bot', message: response.answer}])
+            setCurrentMessage("")
+        } catch (error) {
+            console.error("Error sending message: ", error)
+        }
+
+        revalidatePath(`/chatbot/json/${chatId}/chat`)
+        redirect(`/chatbot/json/${chatId}/history`);
+    }
+
+    function renderChatHistory(chatLog: ChatMessage[]) {
+        if (chatLog) {
+            return chatLog.map((entry: ChatMessage, index: number) => (
+                <div key={index}>
+                    <p>{entry.sender}: {entry.message}</p>
+                </div>
+            ));
+        } else {
+            return ;
+        }
+    }
+
+    return (
+        <div>
+            {renderChatHistory(chatLog)}
+            <ChatInput
+                value={currentMessage}
+                onChange={(e) => setCurrentMessage(e.target.value)}
+                onSubmit={handleSendMessage}
+            />
+        </div>
+    );
+}
